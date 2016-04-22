7b2af5422c1f3b5b35c5b17f89532b7e096390c4
ricky_tran <rickydtran@gmail.com>
2016-04-22 01:00:48 -0400

    Updated Main. Added realm object that acts as container for inputs. Needed Incantations test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <string>
+#include "objects.h"
+#include "incantations.h"
+
+using namespace std;
 
 int main() {
-	
+	int rnum;
+	string realm1, realm2;
+	cin >> rnum;
+	realm *realms[rnum];
+	for(int i = 0; i < rnum; i++) {
+		realms[i] = new realm;
+		cin >> realms[i]->charm >> realms[i]->magi;
+		int power[realms[i]->magi];
+		for(int j = 0; j < realms[i]->magi; j++) {
+			cin >> power[j];
+		}
+		realms[i]->power = power;
+	}
+	cin >> realm1 >> realm2;
+	//test
+	cout << incantationsNeeded(realm1, realm2, realm1.length(), realm2.length()) << endl;
 }
---
+++ objects.h
@@ -0,0 +1,10 @@
+using namespace std;
+#ifndef OBJECTS_H
+#define OBJECTS_H
+class realm {
+public:
+	string charm;
+	int magi;
+	int *power;
+};
+#endif
