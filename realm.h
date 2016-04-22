0df534acb4986a88eb35f21b4b77c06911f6e22c
Ian <ianthemario@hotmail.com>
2016-04-22 18:29:13 -0400

    Added setIncantation

--- realm.h
+++ realm.h
@@ -6,6 +6,7 @@ private:
 	std::string charm;
 	int magi;
 	int *power;
+	int max;
 public:
 	void setCharm(std::string str);
 	std::string getCharm(void);
@@ -13,7 +14,9 @@ public:
 	int getNumOfMagi(void);
 	void setPowerList();
 	int *getPowerList(void);
+	void setMaxIncantations(void);
 	void printRealmData(void);
+
 	//realm(std::string charm, int magi, int *power);
 };
 
@@ -42,6 +45,41 @@ void realm::setPowerList() {
 
 int *realm::getPowerList(void) {
 	return this->power;
+}
+
+void setMaxIncantations(){
+	int maxSubsequence[this->magi];																// Create 1 array of the indicated size
+
+	for (int i = 0; i < this->magi; i++){														// Fill in the indicated magi levels into the array
+		maxSubsequence[i] = 1;																	// Max subsequent increasing for each individual point is 1
+	}
+
+	int currentMax;
+	for (int i = 1; i < this->magi; i++)														// Iterate though all i
+	{
+		currentMax = 1;
+		for ( int j = 0; j <= i;j++) {															// Iterate though all j within i
+
+			if ( currentMax > maxSubsequence[i]){												// If the current max is greater change the respective array location
+				maxSubsequence[i] = currentMax;
+				//cout  << "\n" << "During loop: " << j << " we get =" << maxSubsequence[i]; 
+			}
+			if ( power[j] > power[i] ){															// if the value at j is greater then the value at i continue to next iteration
+				continue;
+			}else {																				// Else increase current max by 1
+				currentMax  = maxSubsequence[j] + 1;
+			}
+			
+
+		}
+	}
+	int maxi = maxSubsequence[0];
+	for ( int g = 1; g < this->magi ; g++){														// Iterate through array and find the largest value
+		if ( maxSubsequence[g] > maxi) maxi = maxSubsequence[g];
+		//cout << "\n"<< "Max at I:" << g << " value = " << maxSubsequence[g];
+	}
+	this->max = maxi;																			// Return largest integer
+
 }
 
 void realm::printRealmData(void) {
